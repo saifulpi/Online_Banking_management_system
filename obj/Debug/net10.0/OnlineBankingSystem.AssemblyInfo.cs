a3e7324d578bc968c97110c3630292469aa9bbf7
saifulpi <saifuldot400@gmail.com>
2026-09-05 00:08:07 +0600

    Fixed errors

--- obj/Debug/net10.0/OnlineBankingSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/OnlineBankingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineBankingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5324209ddc338c6ab6636a1c50ab315d33aaf6ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4928f6806b604ff99340e240add7eb6f8a27aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineBankingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineBankingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
